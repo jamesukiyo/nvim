adce988504b6a938c06c63b04c01d4e32cf2a4ae
James Plummer <154843404+jamesukiyo@users.noreply.github.com>
2024-12-11 23:45:19 +0100

    simple map for telescope (for now)

--- lua/james/lazy/telescope.lua
+++ lua/james/lazy/telescope.lua
@@ -5,7 +5,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
+        local map = vim.keymap.set
+        local builtin = require('telescope.builtin')
         require("telescope").setup({})
+
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     end
 
 }

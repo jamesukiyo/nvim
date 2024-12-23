f6e5e99bfb8ea04dd651a6285b6f2a04639d4b03
James Plummer <154843404+jamesukiyo@users.noreply.github.com>
2024-12-23 22:30:14 +0100

    added native folding with lsp

--- lua/james/set.lua
+++ lua/james/set.lua
@@ -36,3 +36,11 @@ vim.opt.guicursor = ""
 vim.opt.undofile = true
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+vim.o.foldcolumn = '1'
+vim.o.foldenable = true
+vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'expr'

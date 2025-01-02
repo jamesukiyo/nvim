b6e64c7ad7777b630d88b34238d5c2050f83ec35
James Plummer <154843404+jamesukiyo@users.noreply.github.com>
2025-01-02 21:26:31 +0100

    disable autosession restore

--- lua/james/lazy/autosession.lua
+++ lua/james/lazy/autosession.lua
@@ -1,7 +1,9 @@
 return {
     "rmagatti/auto-session",
     config = function()
-        require("auto-session").setup({})
+        require("auto-session").setup({
+            auto_restore = false
+        })
 
         vim.o.sessionoptions="blank,buffers,curdir,folds,terminal,help,tabpages,winsize,winpos,localoptions"
     end,
